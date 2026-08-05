8b1478d07fbd61e5a6b3b2a13d9efac57381d86b
pierce katai <169690632+risingstarfish@users.noreply.github.com>
2026-08-05 18:24:18 -0400

    embed configs

--- include/dotfiles/env.hpp
+++ include/dotfiles/env.hpp
@@ -9,6 +9,17 @@
 
 namespace dotfiles {
 
-constexpr const char user_config[] = CONFIG_JSON "\0";
+namespace fs = std::filesystem;
+
+
+
+constexpr const char default_config[] = {
+#embed "../../configs/default.config.json"
+  , '\0'};
+
+constexpr const char user_config[] = {
+#embed "../../configs/user.config.json"
+  , '\0'};
+
 
 }  // namespace dotfiles
