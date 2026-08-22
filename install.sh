ac04263fdfe71b5a86a1e4d43e3bef93008e703c
pierce katai <169690632+risingstarfish@users.noreply.github.com>
2026-08-21 21:02:43 -0400

    test

--- install.sh
+++ install.sh
@@ -613,7 +613,7 @@ if ((missing_deps > 0)); then
 	log::error "Installation cannot proceed. Exiting..."
 	exit 1
 fi
-
+ 
 log::success "All required files and directories are present."
 
 log::step "Symlinking config files"
