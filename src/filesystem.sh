0bc6e5d6eda2387c18c3664825827bfe1886aa41
pierce katai <169690632+risingstarfish@users.noreply.github.com>
2026-09-21 14:18:10 -0400

    update TODO

--- src/filesystem.sh
+++ src/filesystem.sh
@@ -7,13 +7,16 @@ fi
 readonly __FILESYSTEM_SH_INCLUDED__=1
 
 symlink_all() {
-    :
+    local -n err_ref="${1}"
+
 }
 
 copy_all() {
-    :
+    local -n err_ref="${1}"
+
 }
 
 generate_all() {
-    :
+    local -n err_ref="${1}"
+
 }
